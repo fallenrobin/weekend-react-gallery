c29ddafb5de33fb4bc109a1ab0d258885161b9fd
Juliette Lelchuk <juliette.lelchuk@gmail.com>
2022-03-26 14:33:58 -0500

    add GalleryList component folder, file, and export

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import './App.css';
+import '../modules/gallery.data.js';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import GalleryList from '../GalleryList/GalleryList';
+
+
 
 function App() {
+
+  const [galleryList, setGalleryList] = useState([]);
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
         <p>Gallery goes here</p>
-        <img src="images/squareBeach.jpg"/>
+        {/* GalleryList */}
+        <GalleryList
+        galleryList={galleryList}
+        />
+
       </div>
     );
 }
