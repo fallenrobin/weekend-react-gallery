be08c9f830c052a60515ff5d8400ac5c31678505
Juliette Lelchuk <juliette.lelchuk@gmail.com>
2022-03-26 14:11:52 -0500

    add a couple more pics, give id numbers to each pic object

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,6 +1,14 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }, 
-    { id: 1, path: 'images/squareBeach.jpg', description: 'Me and bae at Ricciolandia', likes: 0 }
+    { id: 1, path: 'images/squareFam', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }, 
+    { id: 2, path: 'images/squareBeach.jpg', description: 'Me and bae at Ricciolandia', likes: 0 }
+    { id: 3, path: 'images/squareDolo.jpg', description: 'Me and bae at Ricciolandia', likes: 0 }
+    { id: 4, path: 'images/squareJammies.jpg', description: 'Me and bae at Ricciolandia', likes: 0 }
+    { id: 5, path: 'images/squareNapoli.jpg', description: 'Me and bae at Ricciolandia', likes: 0 }
+    { id: 6, path: 'images/squareNewborn.jpg', description: 'Me and bae at Ricciolandia', likes: 0 }
+    { id: 7, path: 'images/squareDad.jpg', description: 'Me and bae at Ricciolandia', likes: 0 }
+    { id: 8, path: 'images/squareSFBI.jpg', description: 'Me and bae at Ricciolandia', likes: 0 }
+    { id: 9, path: 'images/squarejTreats.jpg', description: 'Me and bae at Ricciolandia', likes: 0 }
+
 ];
 
 module.exports = galleryItems;
