246b7faf74c39bdf2e09b58173dae4a0ad6feebf
Juliette Lelchuk <juliette.lelchuk@gmail.com>
2022-03-26 14:44:19 -0500

    create GalleryList function and props to pass to GalleryItem

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -11,6 +11,26 @@ function App() {
 
   const [galleryList, setGalleryList] = useState([]);
 
+  // Call function so it runs once on page load
+  // Similar to jQuery's document ready
+  useEffect(() => {
+    fetchGalleryItems();
+  }, [])
+
+  const fetchGalleryItems = () => {
+    axios.get ('/gallery')
+      .then((response) => {
+        console.log('Entire response:', response);
+        // The actual array comes from the data attribute on the response
+        console.log('Just the data:', response.data);
+
+        // Set data into component state
+        setGalleryList(response.data);
+      })
+      .catch(function (error) {
+        console.log('Error on get:', error);
+      });
+
     return (
       <div className="App">
         <header className="App-header">
--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,5 +1,19 @@
+import StudentItem from '../StudentItem/StudentItem';
 
 
+function GalleryList({ galleryList }) {
+    console.log(GalleryList);
+    return (
+        <>
+            {galleryList.map(photo =>
+            (<StudentItem
+                key={photo.id}
+                photo={photo}
+            />)
+            )}
+        </>
+    );
+}
 
 
 
